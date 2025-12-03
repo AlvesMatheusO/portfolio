c760803903344f8aab586914768d0737ffbc4fba
Matheus Alves <matheusalves@unifor.br>
2025-12-03 02:20:00 -0300

    🚧 Work in Progress: navbar aligned

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
+import styles from "./Home.module.css";
 import Image from "next/image";
 import logo from "../assets/logoExample.png";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex">
-      <header>
-        <div className="interface">
+    <main className={`${styles.main} min-h-screen flex`}>
+      <header className={styles.header}>
+        <div className={styles.interface}>
           <div className="logo">
             <a href="#">
-              <Image 
-                src={logo}
-                alt="Logo"
-                width={120}
-                height={120}
-              />
+              <Image src={logo} alt="Logo" width={100} height={100} />
             </a>
           </div>
 
           <nav className="menu-desktop">
             <ul>
-              <li><a href="#">Início</a></li>
-              <li><a href="#">Especialidades</a></li>
-              <li><a href="#">Projetos</a></li>
-              <li><a href="#">Contato</a></li>
+              <li>
+                <a href="#">Início</a>
+              </li>
+              <li>
+                <a href="#">Especialidades</a>
+              </li>
+              <li>
+                <a href="#">Projetos</a>
+              </li>
+              <li>
+                <a href="#">Sobre</a>
+              </li>
             </ul>
           </nav>
 
